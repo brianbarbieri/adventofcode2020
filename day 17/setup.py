752a03aab2c7912b34873798efaaed24dfc90ab0
brianbarbieri <brianbarbieri@hotmail.com>
2021-03-24 21:54:00 +0100

    added conda files

--- day 17/setup.py
+++ day 17/setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
-    name='day 12',
-    ext_modules=cythonize("day 12/solution_1.pyx"),
+    name='day 18',
+    ext_modules=cythonize("day 18/solution_1.pyx"),
     zip_safe=False,
+    include_dirs=[numpy.get_include()]
 )
 
 # python3 setup.py build_ext --inplace
